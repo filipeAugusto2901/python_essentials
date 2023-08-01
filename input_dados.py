886280e982b45c8441d72bc158051b629996693a
filipe2901 <lord-teen@hotmail.com>
2023-07-31 22:08:05 -0300

    Novo arquivo de input de dados

---
+++ input_dados.py
@@ -0,0 +1,8 @@
+#insira para valores serem impressos
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+
+print(f"{nome} tem {idade} anos e {altura} de altura")F

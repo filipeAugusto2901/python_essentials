1578a6e3ee4955efccb841bfe612923ac8a23920
filipe2901 <lord-teen@hotmail.com>
2023-07-31 22:21:10 -0300

    Correção

--- input_dados.py
+++ input_dados.py
@@ -5,4 +5,4 @@ idade = int(input("Digite sua idade: "))
 altura = float(input("Digite sua altura: "))
 
 
-print(f"{nome} tem {idade} anos e {altura} de altura")F
+print(f"{nome} tem {idade} anos e {altura} de altura")
